09f9d120a81c05a2e9cff4a0a46a2952141aa7e1
aarij.siddiqui@live.com <aarij.siddiqui@live.com>
2014-06-09 02:40:30 +0200

    Encoding

--- ClViTra_2.0/src/main/java/de/dbis/services/GetProperty.java
+++ ClViTra_2.0/src/main/java/de/dbis/services/GetProperty.java
@@ -1,5 +1,5 @@
 /* ATLAS Group - Virtual Campfire - www.dbis.rwth-aachen.de
- * Copyright © 2010-2012 Lehrstuhl Informatik V, RWTH Aachen, Germany. All Rights Reserved.
+ * Copyright Â© 2010-2012 Lehrstuhl Informatik V, RWTH Aachen, Germany. All Rights Reserved.
  */
 package de.dbis.services;
 
